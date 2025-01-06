9436ad78c7db28859ba4fad0d4b7fa8319414325
VenerableCode <venerablecode@gmail.com>
2025-01-06 17:05:58 -0600

    Delete KomaruiOSV2 directory

--- KomaruiOSV2/Source/PrimalConfig.h
+++
@@ -1,9 +0,0 @@
-#ifndef PrimalConfig_h
-#define PrimalConfig_h
-
-#import <Foundation/Foundation.h>
-
-// Declare the WritePrimalConfig function
-void WritePrimalConfig();
-
-#endif /* PrimalConfig_h */
